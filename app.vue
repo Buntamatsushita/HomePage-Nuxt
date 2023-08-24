a90b995525efac993865cd7054604dafb8e92b94
Buntako <buntamatsushita@gmail.com>
2023-08-24 10:07:58 +0900

    delete dead code

--- app.vue
+++ app.vue
@@ -11,8 +11,5 @@
 </script>
 
 <style>
-.{
-    margin: 0;
-    padding: 0;
-}
+
 </style>
--- pages/About.vue
+++ pages/About.vue
@@ -1,9 +1,8 @@
 <template>
     <h1>This is About Page </h1>
-    <h2>{{ count }}</h2>
+
     <button >Increment</button>
 </template>
 
 <script lang="ts">
-    var count = 0;
 </script>
