92435fee9f11ebbfb71784896705776c72159fb0
Buntako <buntamatsushita@gmail.com>
2023-08-24 19:23:50 +0900

    add home layouts

---
+++ layouts/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <AppHeader />
+    <main>
+        <slot />
+    </main>
+</template>
+  
+<script lang="ts">
+
+</script>
+  
+<style>
+html {
+    background-color: rgb(243, 252, 253);
+    height: 100vh;
+}
+
+body {
+    margin: 0%;
+}
+
+main {
+    margin: 0%;
+}
+</style>
