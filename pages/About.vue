fabd2fe6b55121f9d4ede6dfddc1ce9993a0bb3b
Buntako <buntamatsushita@gmail.com>
2023-08-24 10:01:04 +0900

    apply layout

---
+++ pages/About.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>This is About Page </h1>
+    <h2>{{ count }}</h2>
+    <button >Increment</button>
+</template>
+
+<script lang="ts">
+    var count = 0;
+</script>
---
+++ pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img src="https://picsum.photos/1920/300" class="main-img" />
+        <h1>This is Home</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+.main-img {
+    object-fit: cover;
+}
+
+h1 {
+    color: red;
+}
+</style>
