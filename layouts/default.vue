7d9f671580d46e4462f65060383c9adea5838a3e
Buntako <buntamatsushita@gmail.com>
2023-08-24 09:57:58 +0900

    define default layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppHeader />
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style>
+html {
+  background-color: black;
+  height: 100vh;
+}
+
+</style>
